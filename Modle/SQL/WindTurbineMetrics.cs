ef5cdae3df2505f1a86bd893f768f5d86b73b1e3
denny.chuang <denny.chuang@LAPTOP-2UUTDATC>
2026-09-22 17:10:25 +0800

    Debug 修正EF Core資料表需要指派鍵值之異常

--- Modle/SQL/WindTurbineMetrics.cs
+++ Modle/SQL/WindTurbineMetrics.cs
@@ -5,11 +5,11 @@ using Microsoft.EntityFrameworkCore;
 
 // 根據您的標籤頁，假設資料表名稱為 WindTurbineMetrics
 [Table("WindTurbineMetrics")]
-// 圖片中未顯示任何主鍵(Key)圖示。如果此表純粹為紀錄用途且無主鍵，必須加上 [Keyless]；若有主鍵，請移除 [Keyless] 並在對應欄位加上 [Key]
-[Keyless] 
 public class WindTurbineMetric
 {
-    // 因為圖片中所有欄位都允許 Null，所以 C# 型別全數加上 ? 設為 Nullable
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; } // 補上主鍵
     
     [Column(TypeName = "varchar(15)")]
     public string? FarmId { get; set; }
